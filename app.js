ee78c44b5ee83a11ae050a1826d79839a073f463
Mario Olinto <marioolinto@gmail.com>
2018-03-25 00:16:26 -0400

    Add invalid endpoint message on ( / )

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
+});
+
 app.listen(port, () => {
     console.log("Server started on port "+port);
 });
