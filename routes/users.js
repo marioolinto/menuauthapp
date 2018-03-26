6c0b87d2544021d617f073b0f009f0b1d4c3b90a
Mario Olinto <marioolinto@gmail.com>
2018-03-25 20:02:40 -0400

    Implemented the endpoint register by method post on user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 //Register
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+    let newUser = User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err){
+            res.json({ success: false, message: 'Failed to register user'});            
+        } else {
+            res.json ({ success: true, message: ''});
+        }
+    });
 });
 
 //Authenticate
